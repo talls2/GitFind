30106b3f2f3ad2c5d7628a2fa36f66de2fc86217
talls2 <williambenet75@gmail.com>
2022-10-12 20:29:11 -0300

    ˜changes˜

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,11 +7,25 @@ export default function App() {
     <div className="App">
       <Header />
       <div className="conteudo">
-        <img
-          src={background}
-          className="background"
-          alt="background github image"
-        />
+        <img src={background} className="background" alt="background github" />
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username" />
+            <button>Buscar</button>
+          </div>
+          <div className="perfil">
+            <img
+              src="https://avatars.githubusercontent.com/u/36342788?v=4"
+              className="profile"
+              alt="profile-pic"
+            />
+            <div>
+              <h3>William Benett</h3>
+              <span>Descriçāo</span>
+              <p>Descriçāo</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
